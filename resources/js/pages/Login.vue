67aaf180ebcec9f228c47a0017ff5b10bef5ed73
Bue <myatbhonemaw72@gmail.com>
2025-08-16 23:19:14 +0630

    add:vue-core

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,9 +1,26 @@
 <template>
-    <div>
-        <DesktopLogin />
-    </div>
+  <div v-if="isMobile">
+    <MobileLogin />
+  </div>
+  <div v-else-if="isTabletOrDesktop">
+    <DesktopLogin />
+  </div>
 </template>
 
 <script setup>
+import { useBreakpoints } from '@vueuse/core'
 import DesktopLogin from '../components/Desktop/login.vue'
+import MobileLogin from '../components/Mobile/login.vue'
+// Define your custom breakpoints
+const breakpoints = useBreakpoints({
+  sm: 640,
+  md: 768,
+  lg: 1024,
+})
+
+// Mobile < 768px
+const isMobile = breakpoints.smaller('md')
+
+// Tablet or Desktop ≥ 768px
+const isTabletOrDesktop = breakpoints.greaterOrEqual('md')
 </script>
