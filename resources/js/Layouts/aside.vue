18be3191f04a383610ec7114ea1f1ed8a6ab9d14
Bue <myatbhonemaw72@gmail.com>
2025-08-24 22:56:00 +0630

    aside-layout

--- resources/js/Layouts/aside.vue
+++ resources/js/Layouts/aside.vue
@@ -1,24 +1,40 @@
 <template>
-    <div v-for="(d , i) in data" :key="i" class="py-3">
-        <router-link :to="d.path" class="px-5 rounded py-2 bg-gray-400">
-            {{ d . name }}
-        </router-link>
-    </div>
+  <router-link
+    v-for="(d, i) in data"
+    :key="i"
+    :to="d.path"
+    class="block text-center py-3 w-full"
+    :class="{
+      'bg-purple-600 rounded border-b border-cyan-300': active === d.slug,
+      'text-cyan-300': active !== d.slug
+    }"
+  >
+    {{ d.name }}
+  </router-link>
 </template>
-<script setup>
-    import {
-        ref
-    } from 'vue';
 
-    const data = ref([{
-            name: 'Home',
-            path: '/',
-            slug: 'home'
-        },
-        {
-            name: 'Create User',
-            path: '/user-create',
-            slug: 'user-create'
-        }
-    ]);
+<script setup>
+import { ref, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+
+// သတ်မှတ်ထားတဲ့ tab data
+const data = ref([
+  { name: 'Home Page', path: '/', slug: 'home' },
+  { name: 'Create User', path: '/user-create', slug: 'user-create' }
+])
+
+const active = ref('')
+
+// vue-router မှ current route ကိုရယူ
+const route = useRoute()
+
+// လက်ရှိ route path အပေါ်မူတည်ပြီး active tab ကိုပြောင်း
+watchEffect(() => {
+  const matched = data.value.find((item) => item.path == route.path)
+  if (matched) {
+    active.value = matched.slug
+  } else {
+    active.value = ''
+  }
+})
 </script>
