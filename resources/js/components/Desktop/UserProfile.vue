5b1b7cb8e9c107d02c896952a748e3f8c211be7b
Bue <myatbhonemaw72@gmail.com>
2025-08-29 21:47:18 +0630

    update:profile

--- resources/js/components/Desktop/UserProfile.vue
+++ resources/js/components/Desktop/UserProfile.vue
@@ -4,11 +4,11 @@
             <div class="mx-10 text-cyan-400 overflow-hidden shadow-xl rounded-xl border border-cyan-700">
                 <div class="px-6 py-6 bg-gray-900 border-b border-cyan-700 rounded-t-xl">
                     <h3 class="text-xl font-semibold text-cyan-300">
-                        User Profile
+                        Bue Profile
                     </h3>
-                    <p class="mt-1 text-sm text-cyan-500">
+                    <!-- <p class="mt-1 text-sm text-cyan-500">
                         This is some information about the user.
-                    </p>
+                    </p> -->
                 </div>
                 <div class="px-6 py-5">
                     <dl class="divide-y divide-cyan-800">
