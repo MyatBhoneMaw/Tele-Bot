8a7fada5e9e6139d37721bce34bf6622ae3134f5
Bue <myatbhonemaw72@gmail.com>
2025-08-29 21:36:28 +0630

    anniation-tab

--- resources/js/components/Desktop/Plan.vue
+++ resources/js/components/Desktop/Plan.vue
@@ -1,59 +1,70 @@
 <template>
     <Layout>
-        <div class="py-4 px-4">
-            <div class="max-w-5xl mx-auto">
-                <button v-for="tab in tabs" :key="tab.key" @click="tabChange(tab.key)" class="cursor-pointer"
-                    :class="['px-4 py-2', active === tab.key ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black']">
-                    {{ tab . name }}
-                </button>
-
-
-            </div>
-            <div class="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <UserCard v-for="(user, index) in users" :key="index" :data="user" />
-            </div>
+        <div class="flex max-w-5xl mx-auto space-x-4 border-gray-700 px-4 py-5 cursor-pointer">
+            <button v-for="tab in tabs" :key="tab.key" @click="tabChange(tab.key)"
+                class="relative px-4 py-2 font-medium transition duration-300 ease-in-out cursor-pointer"
+                :class="active === tab.key ? 'text-cyan-400' : 'text-white hover:text-cyan-300'">
+                {{ tab . name }}
+                <!-- Animated underline -->
+                <span class="absolute left-0 -bottom-1 h-0.5 bg-cyan-400 transition-all duration-300 ease-in-out"
+                    :class="active === tab.key ? 'w-full' : 'w-0'"></span>
+            </button>
+        </div>
+        <div class="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <UserCard v-for="(user, index) in users" :key="index" :data="user" />
         </div>
     </Layout>
 </template>
 
-
-
 <script setup>
-import Layout from '@/Layouts/layout.vue';
-import { onMounted, ref, watch } from 'vue';
-import UserCard from './component/UserCard.vue';
-import { get } from '@/utils/api';
+    import Layout from '@/Layouts/layout.vue';
+    import {
+        onMounted,
+        ref,
+        watch
+    } from 'vue';
+    import UserCard from './component/UserCard.vue';
+    import {
+        get
+    } from '@/utils/api';
 
-const users = ref([]);
-const active = ref('');
+    const users = ref([]);
+    const active = ref('');
 
-const tabs = ref([
-  { name: '15K Plan', key: '15K' },
-  { name: '25K Plan', key: '25K' }
-]);
+    const tabs = ref([{
+            name: '15K Plan',
+            key: '15K'
+        },
+        {
+            name: '25K Plan',
+            key: '25K'
+        }
+    ]);
 
-const getBuyUser = async () => {
-  try {
-    const data = await get('/users', { plan: active.value });
-    if (data) {
-      users.value = data.data;
-    }
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response);
-    }
-  }
-};
+    const getBuyUser = async () => {
+        try {
+            const data = await get('/users', {
+                plan: active.value
+            });
+            if (data) {
+                users.value = data.data;
+            }
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response);
+            }
+        }
+    };
 
-const tabChange = (key) => {
-  active.value = key;
-};
+    const tabChange = (key) => {
+        active.value = key;
+    };
 
-watch(active, () => {
-  if (active.value) getBuyUser();
-});
+    watch(active, () => {
+        if (active.value) getBuyUser();
+    });
 
-onMounted(() => {
-  active.value = '15K';
-});
+    onMounted(() => {
+        active.value = '15K';
+    });
 </script>
