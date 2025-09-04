5c1f00fa92ab205572d41050fae74bf702b918b8
Bue <myatbhonemaw72@gmail.com>
2025-09-04 22:53:05 +0630

    route middleware

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,53 +2,67 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/pages/home.vue';
 import Login from '@/pages/login.vue';
-import Create from '@/pages/create.vue'
+import Create from '@/pages/create.vue';
 import UserProfile from '../pages/userProfile.vue';
 import Plan from '../pages/plan.vue';
 import Employee from '../pages/employee.vue';
 import PageNotFound from '../pages/404.vue';
 
 const routes = [
-{
-path: '/',
-name: 'Home',
-component: Home,
-},
-{ 
-  path: '/:pathMatch(.*)*',
-  name: 'NotFound',
-  component: PageNotFound
-},
-{
-path : '/login',
-name : Login,
-component : Login
-},
-{
-path : '/user-create',
-name : 'Create',
-component : Create
-},
-{
-path : '/user-profile',
-name : 'UserProfile',
-component : UserProfile
-},
-{
-path : '/plan',
-name : 'Plan',
-component : Plan
-},
-{
-path : '/employee',
-name : 'Employee',
-component : Employee
-}
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  { 
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/user-create',
+    name: 'Create',
+    component: Create
+  },
+  {
+    path: '/user-profile',
+    name: 'UserProfile',
+    component: UserProfile
+  },
+  {
+    path: '/plan',
+    name: 'Plan',
+    component: Plan
+  },
+  {
+    path: '/employee',
+    name: 'Employee',
+    component: Employee
+  }
 ];
 
 const router = createRouter({
-history: createWebHistory(),
-routes,
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const user = localStorage.getItem('user');
+
+  if (to.path == '/login') {
+    return next();
+  }
+
+  if (!user) {
+    return next('/login');
+  }
+
+  next();
 });
 
 export default router;
